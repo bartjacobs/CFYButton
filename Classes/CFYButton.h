5dc45034df3d3d9018cbacc491877071d6be53eb
Bart Jacobs <developer@bartjacobs.com>
2015-10-26 11:42:48 +0100

    Pod setup

---
+++ Classes/CFYButton.h
@@ -0,0 +1,13 @@
+//
+//  CFYButton.h
+//  CFYButton
+//
+//  Created by Bart Jacobs on 21/09/15.
+//  Copyright © 2015 Code Foundry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CFYButton : UIButton
+
+@end
---
+++ Classes/CFYButton.m
@@ -0,0 +1,13 @@
+//
+//  CFYButton.m
+//  CFYButton
+//
+//  Created by Bart Jacobs on 21/09/15.
+//  Copyright © 2015 Code Foundry. All rights reserved.
+//
+
+#import "CFYButton.h"
+
+@implementation CFYButton
+
+@end
